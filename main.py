b5e9df2836ec010ac68f63cfa43786b653726586
trisha-pal0 <140142936+trisha-pal0@users.noreply.github.com>
2023-07-21 15:27:03 +0530

    code for detection algorithm

--- main.py
+++ main.py
@@ -199,3 +199,73 @@ if __name__ == "__main__":
 
     feature_matching(image1_path, image2_path)
 
+
+
+import cv2
+import numpy as np
+
+# Load YOLO model and class names
+net = cv2.dnn.readNet("yolov3.cfg", "yolov3.weights")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# Get output layer names
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# Load image and get dimensions
+image = cv2.imread("your_image.jpg")  # Replace with the path to your image
+height, width, channels = image.shape
+
+# Prepare image for YOLO model
+blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+net.setInput(blob)
+
+# Get detections
+outs = net.forward(output_layers)
+
+# Process detections
+class_ids = []
+confidences = []
+boxes = []
+
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:  # You can adjust this threshold based on your needs
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+
+            # Rectangle coordinates
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+# Non-maximum suppression to remove duplicate detections
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+# Draw the detections on the image
+for i in range(len(boxes)):
+    if i in indexes:
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        confidence = confidences[i]
+        color = (0, 255, 0)  # Green color for the bounding box
+        cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(image, f"{label} {confidence:.2f}", (x, y - 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+# Show the output image
+cv2.imshow("Object Detection", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
